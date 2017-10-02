0655b5788a0ae3c01534243d64c69b9422204fe9
Темирлан <tim4kz@gmail.com>
2017-10-02 15:36:06 +0600

    ex1 butaginda hello.java jane MyMath.java filedarina ozgerister engizildi

--- src/kz/kaznitu/hello.java
+++ src/kz/kaznitu/hello.java
@@ -5,5 +5,13 @@ public class hello {
         System.out.println("HELLO!!");
         System.out.println("bul zer jumistin maksati:");
         System.out.println("GitHub pen jumis jasap uirenu.");
+
+        int a=MyMath.kvadrat(5);
+        int b=MyMath.kub(5);
+
+        System.out.println("a="+a);
+        System.out.println("b="+b);
+
+        System.out.println(MyMath.max(8,9));
     }
 }
