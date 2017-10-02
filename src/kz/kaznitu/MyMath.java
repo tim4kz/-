b5d4b01f4c89abdc66e6daf8b161e635fe3dd47b
Темирлан <tim4kz@gmail.com>
2017-10-02 17:05:59 +0600

    MyMath.java filena isOdd() metodina ozgeris engizdim int emes boolean kaitaradi

--- src/kz/kaznitu/MyMath.java
+++ src/kz/kaznitu/MyMath.java
@@ -2,36 +2,40 @@ package kz.kaznitu;
 
 public class MyMath {
     public static int kvadrat(int a) {
-        return a*a;
+        return a * a;
     }
 
-    public static int kub(int a){
-        return a*a*a;
+    public static int kub(int a) {
+        return a * a * a;
     }
 
-    public static boolean isEven(int a){
-        return (a%2==0);
+    public static boolean isEven(int a) {
+        return (a % 2 == 0);
     }
 
-    public static int max(int a,int b) {
+    public static int max(int a, int b) {
         if (a >= b)
             return b;
     }
 
-    public static int min(int a,int b){
-        if (a>=b)
+    public static int min(int a, int b) {
+        if (a >= b)
             return b;
         else
             return a;
     }
-    public static int max3(int a,int b,int c){
-        int max=max(a,b);
-        return max(max,c);
+
+    public static int max3(int a, int b, int c) {
+        int max = max(a, b);
+        return max(max, c);
     }
-    public static int min3(int a,int b,int c){
-        int min=min(a,b);
-        return min(min,c);
+
+    public static int min3(int a, int b, int c) {
+        int min = min(a, b);
+        return min(min, c);
     }
-    public static isOdd(int a){
-        return (a%2!=0);
+
+    public static boolean isOdd(int a) {
+        return (a % 2 != 0);
+    }
 }
