427c1344f9e16b317b4956adc760cc83cfe32d4f
Темирлан <tim4kz@gmail.com>
2017-10-02 16:31:30 +0600

    ex1 butaginda hello.java  ozgerister engizildi

--- src/kz/kaznitu/hello.java
+++ src/kz/kaznitu/hello.java
@@ -6,12 +6,7 @@ public class hello {
         System.out.println("bul zer jumistin maksati:");
         System.out.println("GitHub pen jumis jasap uirenu.");
 
-        int a=MyMath.kvadrat(5);
-        int b=MyMath.kub(5);
-
-        System.out.println("a="+a);
-        System.out.println("b="+b);
-
-        System.out.println(MyMath.max(8,9));
+        System.out.println("max="+MyMath.max3(8,9,12));
+        System.out.println("min="+MyMath.min3(12,8,15));
     }
 }
