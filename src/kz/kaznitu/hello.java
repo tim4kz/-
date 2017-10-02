3c0cbc5b14e07e6543768e0c7bb05057f8a16b36
Темирлан <tim4kz@gmail.com>
2017-10-02 13:33:12 +0600

    "ekinwi commit"

--- src/kz/kaznitu/hello.java
+++ src/kz/kaznitu/hello.java
@@ -2,6 +2,8 @@ package kz.kaznitu;
 
 public class hello {
     public static void main(String[] args) {
-        System.out.print("HELLO!!");
+        System.out.println("HELLO!!");
+        System.out.println("bul zer jumistin maksati:");
+        System.out.println("GitHub pen jumis jasap uirenu.");
     }
 }
